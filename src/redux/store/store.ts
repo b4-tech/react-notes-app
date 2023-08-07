9f88ca11da6c5093721ef05ae8605258caeb56dd
b4-tech <hcetanutrof@gmail.com>
2023-08-07 22:15:26 +0200

    feat: Integrate  for state persistence
    Signed-off-by: b4-tech <hcetanutrof@gmail.com>

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,15 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import notesReducer from '../slices/notesSlice'
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "./types";
 import viewModeReducer from '../slices/viewModeSlice';
+import { persistReducer, persistStore } from "redux-persist";
+import storage from 'redux-persist/es/storage';
+
+const reducers = {
+	notes: notesReducer,
+	viewMode: viewModeReducer
+}
+
+const rootReducer = combineReducers(reducers);
+
+const persistConfig = {
+	key: 'root',
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-	reducer: {
-		notes: notesReducer,
-		viewMode: viewModeReducer
-	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: false,
+		}),
+	reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
